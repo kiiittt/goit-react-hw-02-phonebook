324426a032a0e642cd552c7c5a55c903055073bb
Darya <daryakolisnik@gmail.com>
2023-05-29 13:43:32 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
-import PropTypes from 'prop-types';
 
 export class ContactForm extends Component {
   state = {
